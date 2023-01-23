ee30ea5e6dce29cbc0ef084928870e81360dd393
Ghanendra K Das <gkdas2@illinois.edu>
2023-01-22 20:36:06 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,9 @@
 #include "../include/add.h"
 using namespace std;
 
-int main(int argc, char **argv){
+int main(int argc, char **argv)
+{
     int a = add(2, 3);
-   std::cout << a << std::endl;
+    std::cout << a << std::endl;
     return 0;
 }
